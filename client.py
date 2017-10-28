4071f1266560c90e6c8c818035e17965b47cbe40
Brad Tucker <brad.victor.tucker@gmail.com>
2017-10-28 14:24:24 -0700

    flesh out server and client code

---
+++ client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import socket
+import threading
+import json
+
+#client interface
+class client():
+    #set configuration variables
+    address = ('127.0.0.1', 6500)
+
+    #initialize the client
+    def __init__(self):
+        #connect a socket on the address
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(self.address)
+
+        #data collection defaults to off
+        self.running = False
+
+        #variable for collected data
+        self.data = {}
+
+        #commands for server
+        self.send_request = 'send'.encode()
+        self.start_request = 'start'.encode()
+        self.stop_request = 'stop'.encode()
+
+    #request data to process and process it constantly
+    def process(self):
+        while self.running:
+            self.sock.send(self.send_request)
+            self.data = json.loads(self.sock.recv(4096).decode())
+
+    #start data collection
+    def start(self):
+        #enable the collection
+        self.running = True
+        self.sock.send(self.start_request)
+        print(self.sock.recv(4096).decode())
+
+        #start data processing in background
+        t = threading.Thread(target = self.process)
+        t.start()
+
+    #stop the data collections and shut down server
+    def stop(self):
+        self.running = False
+        self.sock.send(self.stop_request)
--- server.py
+++ server.py
@@ -1,30 +1,86 @@
 #!/usr/bin/env python3
 import socket
+import threading
+import json
 
-port = 5000
+#rigging data server
+class server():
+    #set configuration variables
+    port = 6500
+    sensors = {'rt':0x68, 'lt':0x01, 'rs':0x02, 'ls':0x03}
+    registers = {'ax':0x3B, 'ay':0x3D, 'az':0x3f, 'gx':0x43, 'gy':0x45, 'gz':0x47}
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #initialize the server
+    def __init__(self):
+        #make socket listening on port
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', self.port))
+        s.listen(1)
 
-sock.bind(('', port))
+        #make new socket accepting a connection
+        self.sock, addr = s.accept()
 
-sock.listen(1)
+        #start with no data collection active
+        self.running = False
 
-s, address = sock.accept()
+        #accepted commands
+        self.send_request = 'send'.encode()
+        self.start_request = 'start'.encode()
+        self.stop_request = 'stop'.encode()
 
-s.send('Connected'.encode())
+        #start listening for commands
+        self.listen()
 
-running = True
+    #accept commands
+    def listen(self):
+        #loop until listening is set to false
+        listening = True
+        while listening:
+            #accept a command from the client
+            command = self.sock.recv(2048)
 
-while running:
-    command = s.recv(1024).decode()
+            #start processing data in the background
+            if command == self.start_request:
+                t = threading.Thread(target = self.process)
+                ####t.start()
+                self.sock.send('started'.encode())
 
-    if command == 'exit':
-        running = False
-    elif command == 'send':
-        pass
-    elif command == 'record':
-        pass
-    elif command == 'stop':
-        pass
-    else:
-        print(command)
+            #transmit current data values
+            elif command == self.send_request:
+                #pack the data in a standard format json
+                self.sock.send(json.dumps(self.pack()).encode())
+
+            #end data collection and shutdown (if stop request)
+            else:
+                listening = False
+                self.running = False
+                self.sock.send(''.encode())
+
+    #read from sensors and integrate on a loop
+    def process(self):
+        #loop until server is shut down
+        while self.running:
+            pass
+
+    #pack data into one dictionary for json dump
+    def pack(self):
+        return {'t':0, 's1':(1,2,3), 's2':(4,5,6), 's3':(7,8,9)}
+
+    #read short using sensor address and register address
+    def get_register_data(self, sensor, register):
+        #get high and low bytes
+        h = 255 ####self.bus.read_byte_data(sensor, register)
+        l = 128 ####self.bus.read_byte_data(sensor, register + 1)
+
+        #move high byte up 8 digits and add low byte
+        value = (h << 8) + l
+
+        #convert from unsigned to signed short
+        if value >= 0x8000:
+            value -= 0x10000
+
+        #return signed short
+        return value
+
+#start a new server object
+server()
