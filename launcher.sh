013dd46c723b56750bde64cd4119952098a31668
brad <brad.victor.tucker@gmail.com>
2017-11-18 10:51:01 -0800

    make i2c start at boot and display attached devices

--- launcher.sh
+++ launcher.sh
@@ -17,6 +17,14 @@ sudo nmcli connection modify $CON 802-11-wireless.mode ap 802-11-wireless.band b
 echo "Broadcasting Network"
 sudo nmcli connection up $CON
 
+#enable i2c
+echo "Starting I2C Bus"
+sudo dtparam i2c_arm=on
+
+#test i2c for connected devices
+echo "Testing I2C Addresses"
+i2cdetect -y 1
+
 #run the server code
 echo "Starting Server"
 #python3 server.py
