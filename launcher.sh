78966f7af0275378ebe8a19e2f929ef1db55f018
brad <brad.victor.tucker@gmail.com>
2017-11-17 18:17:53 -0800

    revert

--- launcher.sh
+++ launcher.sh
@@ -11,7 +11,7 @@ sudo nmcli connection add type wifi ifname $WIFI con-name $CON autoconnect no ss
 
 #change the connection type to a hotspot
 echo "Modifying Connection Settings"
-sudo nmcli connection modify $CON 802-11-wireless.mode ap 802-11-wireless.band a ipv4.method shared
+sudo nmcli connection modify $CON 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 
 #turn on the connection
 echo "Broadcasting Network"
