5b6fa3f3ab6f99689c389282b7e1259bc11335e9
Brad Tucker <brad.victor.tucker@gmail.com>
2017-11-19 09:55:34 -0800

    clean up plots and code

--- joint_display.m
+++ joint_display.m
@@ -2,6 +2,48 @@
 clc
 clearvars
 
+%make unit vector for limbs
+u = [0;1;0];
+
+%initialize data
+xdata = [0, 0, 0];
+ydata = [0, 1, 2];
+zdata = [0, 0, 0];
+kdata = 0;
+tdata = 0;
+
+%log data into one matrix
+data_log = [0, 180];
+
+%set up large figure
+figure('Position', [200, 200, 1200, 500])
+
+%plot data in three dimentions
+subplot(1,2,1)
+fig1 = plot3(zdata, xdata, ydata, 'b-o');
+xlabel('z')
+ylabel('x')
+zlabel('y')
+axis([-2, 2, -2, 2, 0, 4])
+grid on
+
+%plot knee angle
+subplot(1,2,2)
+fig2 = plot(tdata, kdata, 'r-');
+grid on
+axis([-10, 0, 0, 90])
+xlabel('t - t_f')
+ylabel('Knee Angle')
+
+%set data sources to update 3D plot
+fig1.XDataSource = 'zdata';
+fig1.YDataSource = 'xdata';
+fig1.ZDataSource = 'ydata';
+
+%set data sources for knee angle plot
+fig2.XDataSource = 'tdata';
+fig2.YDataSource = 'kdata';
+
 %add current path to python to load client
 if count(py.sys.path, pwd) == 0
     insert(py.sys.path,int32(0),pwd);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
@@ -13,102 +55,60 @@ client = py.importlib.import_module('client').client();
 %start the client and get start time in seconds from epoch
 t0 = client.start();
 
-%make unit vector for limbs
-u = [0;1;0];
-
-%initialize data
-xdata = [0, 0, 0];
-ydata = [0, 1, 2];
-zdata = [0, 0, 0];
-kdata = 180;
-tdata = 0;
-
-data_log = [0, 180];
-
-%set up large figure
-figure('Position', [100, 200, 1200, 600])
-
-%plot data in three dimentions
-subplot(1,2,1)
-fig1 = plot3(xdata, ydata, zdata, 'b-o');
-xlabel('x')
-ylabel('y')
-zlabel('z')
-axis([-2, 2, -2, 2, 0, 4])
-grid on
-
-%plot knee angle
-subplot(1,2,2)
-fig2 = plot(tdata, kdata, 'r-');
-grid on
-axis([-10, 0, 0,180])
-xlabel('t')
-ylabel('Knee Angle')
-
-%set data sources to update graph properly
-fig1.XDataSource = 'xdata';
-fig1.ZDataSource = 'ydata';
-fig1.YDataSource = 'zdata';
-
-fig2.XDataSource = 'tdata';
-fig2.YDataSource = 'kdata';
-
-%add text info
-text = uicontrol('Style', 'text', 'HorizontalAlignment', 'left', 'Position', [10, 10, 200, 20]);
-
 %loop while running
 running = true;
 while running
     
     %get data from python client
     t = client.get('t');
-    x = client.get('rf', 0)*pi/180;
-    y = client.get('rf', 1)*pi/180;
-    z = client.get('rf', 2)*pi/180;
+    x1 = client.get('rf', 0);
+    y1 = client.get('rf', 1);
+    z1 = client.get('rf', 2);
+    x2 = client.get('rt', 0);
+    y2 = client.get('rt', 1);
+    z2 = client.get('rt', 2);
 
     %calculate rotation matrix for each data value
-    rx = [1, 0, 0; 0, cos(x), -sin(x); 0, sin(x), cos(x)];
-    ry = [cos(y), 0, sin(y); 0, 1, 0; -sin(y), 0, cos(y)];
-    rz = [cos(z), -sin(z), 0; sin(z), cos(z), 0; 0, 0, 1];
+    rx1 = [1, 0, 0; 0, cosd(x1), -sind(x1); 0, sind(x1), cosd(x1)];
+    ry1 = [cosd(y1), 0, sind(y1); 0, 1, 0; -sind(y1), 0, cosd(y1)];
+    rz1 = [cosd(z1), -sind(z1), 0; sind(z1), cosd(z1), 0; 0, 0, 1];
+    
+    %calculate rotation matrix for vector 2
+    rx2 = [1, 0, 0; 0, cosd(x2), -sind(x2); 0, sind(x2), cosd(x2)];
+    ry2 = [cosd(y2), 0, sind(y2); 0, 1, 0; -sind(y2), 0, cosd(y2)];
+    rz2 = [cosd(z2), -sind(z2), 0; sind(z2), cosd(z2), 0; 0, 0, 1];
 
     %calculate vector 1
-    v1 = rz*ry*rx*u;
-
-    %get data from python client
-    x = client.get('rt', 0)*pi/180;
-    y = client.get('rt', 1)*pi/180;
-    z = client.get('rt', 2)*pi/180;
-
-    %calculate rotation matrix for each data value
-    rx = [1, 0, 0; 0, cos(x), -sin(x); 0, sin(x), cos(x)];
-    ry = [cos(y), 0, sin(y); 0, 1, 0; -sin(y), 0, cos(y)];
-    rz = [cos(z), -sin(z), 0; sin(z), cos(z), 0; 0, 0, 1];
+    v1 = rz1*ry1*rx1*u;
 
     %calculate vector 2
-    v2 = rz*ry*rx*u;
+    v2 = rz2*ry2*rx2*u;
     
     %calculate angle of knee from vectors
-    knee = 180 - acos(dot(v1,v2)/norm(v1)/norm(v2))*180/pi;
+    knee = acosd(dot(v1,v2)/norm(v1)/norm(v2));
 
     %add data to log
     data_log = [data_log; [t-t0, knee]];
     
-    %update data
+    %update 3D plot data
     xdata = [0, v1(1), v2(1)+v1(1)];
     ydata = [0, v1(2), v2(2)+v1(2)];
     zdata = [0, v1(3), v2(3)+v1(3)];
+    
+    %update knee plot data
     tdata = data_log(1 + end - min(end, 200):end, 1) - t + t0;
     kdata = data_log(1 + end - min(end, 200):end, 2);
     
     %try to update and draw to figure
     try
-        %set(text, 'String', strjoin(['Knee: ', string(knee)]))
+        %refresh plot data and drawnow using pause
         refreshdata(fig1, 'caller')
         refreshdata(fig2, 'caller')
         pause(0.050)
         
     %stop client and loop if figure has been closed
     catch
+        %stop everything and output data log
         client.stop()
         running = false;
         disp(data_log);
