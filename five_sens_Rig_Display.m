5916c1a9a3683f9d216a6ce8214f6fd58c99af37
gafigeldy <33469025+gafigeldy@users.noreply.github.com>
2018-04-17 01:11:37 -0700

    Update Matlab
    Add new file to be edited for 5 sensors. Kept 2 sensor file the same. Add file to convert to csv format

--- five_sens_Rig_Display.m
+++ five_sens_Rig_Display.m
@@ -22,7 +22,6 @@ zlabel('y')
 axis([-2, 2, -2, 2, 0, 4])
 grid on
 
-
 %set data sources to update 3D plot
 fig1.XDataSource = 'zdata';
 fig1.YDataSource = 'xdata';
@@ -37,7 +36,12 @@ end
 client = py.importlib.import_module('client').client();
 
 %start the client and get start time in seconds from epoch
-t0 = client.start();
+t0 = client.start(); 
+
+%initialize data for data recording, add titles to columns
+row_count = 1;
+syms time rfemurx rfemury rfemurz lfemurx lfemury lfemurz rtibiax rtibiay rtibiaz ltibiax ltibiay ltibiaz torsox torsoy torsoz
+data_matrix = [' ' , time, rfemurx, rfemury, rfemurz, lfemurx, lfemury, lfemurz, rtibiax, rtibiay, rtibiaz, ltibiax, ltibiay, ltibiaz, torsox, torsoy, torsoz];
 
 %loop while running
 running = true;
@@ -51,7 +55,16 @@ while running
     x2 = client.get('lf', 0);
     y2 = client.get('lf', 1);
     z2 = client.get('lf', 2);
- 
+    x3 = 0;
+    y3 = 0;
+    z3 = 0;
+    x4 = 0;
+    y4 = 0;
+    z4 = 0;
+    x5 = 0;
+    y5 = 0;
+    z5 = 0;
+
     %calculate rotation matrix for each data value
     rx1 = [1, 0, 0; 0, cosd(x1), -sind(x1); 0, sind(x1), cosd(x1)];
     ry1 = [cosd(y1), 0, sind(y1); 0, 1, 0; -sind(y1), 0, cosd(y1)];
@@ -62,17 +75,45 @@ while running
     ry2 = [cosd(y2), 0, sind(y2); 0, 1, 0; -sind(y2), 0, cosd(y2)];
     rz2 = [cosd(z2), -sind(z2), 0; sind(z2), cosd(z2), 0; 0, 0, 1];
     
+    %calculate rotation matrix for vector 3
+    rx3 = [1, 0, 0; 0, cosd(x3), -sind(x3); 0, sind(x3), cosd(x3)];
+    ry3 = [cosd(y3), 0, sind(y3); 0, 1, 0; -sind(y3), 0, cosd(y3)];
+    rz3 = [cosd(z3), -sind(z3), 0; sind(z3), cosd(z3), 0; 0, 0, 1];
+           
+    %calculate rotation matrix for vector 3
+    rx4 = [1, 0, 0; 0, cosd(x4), -sind(x4); 0, sind(x4), cosd(x4)];
+    ry4 = [cosd(y4), 0, sind(y4); 0, 1, 0; -sind(y4), 0, cosd(y4)];
+    rz4 = [cosd(z4), -sind(z4), 0; sind(z4), cosd(z4), 0; 0, 0, 1];
+        
+    %calculate rotation matrix for vector 3
+    rx5 = [1, 0, 0; 0, cosd(x5), -sind(x5); 0, sind(x5), cosd(x5)];
+    ry5 = [cosd(y5), 0, sind(y5); 0, 1, 0; -sind(y5), 0, cosd(y5)];
+    rz5 = [cosd(z5), -sind(z5), 0; sind(z5), cosd(z5), 0; 0, 0, 1];
+
     %calculate vector 1 for left femur
     lfv = rx1*rz1*[0;1;0];
 
     %calculate vector 2 for right femur
     rfv = rx1*rz2*[0;1;0];
-        
+    
+    %calcualte vector 3 for left tibia
+    ltv = rx3*rz3*[0;1;0];
+    
+    %calculate vector 4 for right tibia
+    rtv = rx4*rz4*[0;1;0];
+    
+    %calculate vector 5 for torso
+    tv = rx5*rz5*[0;1;0];    
+       
     %update 3D plot data
-    xdata = [0, 0, 0, 0, lfv(1), rfv(1)+lfv(1)];
-    ydata = [0, 1, 2, 2, 2-lfv(2), 2-rfv(2)-lfv(2)];
-    zdata = [.25, .25, .25, -.25, -.25+lfv(3), -.25+rfv(3)+lfv(3)];
-
+    xdata = [rfv(1)+rtv(1), rfv(1), 0, 0, lfv(1), lfv(1)+ltv(1)];
+    ydata = [2-rfv(2)-rtv(2), 2-rfv(2), 2, 2, 2-lfv(2), 2-lfv(2)-ltv(2)];
+    zdata = [.25+rfv(3)+rtv(3), .25+rfv(3), .25, -.25, -.25+lfv(3), -.25+lfv(3)+lfv(3)];
+    
+    %Concactinate new data pull to data matrix
+    next_row = [row_count, t x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, x5, y5, z5];
+    data_matrix = [data_matrix; next_row];
+    row_count = row_count+1;
     
     %try to update and draw to figure
     try
@@ -87,4 +128,5 @@ while running
         running = false;
         disp(data_log);
     end
+    
 end
