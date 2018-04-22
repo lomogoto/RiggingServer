c40795c8145e08a9d269c0e99a63f5db4796e29d
brad <brad.victor.tucker@gmail.com>
2018-04-22 16:10:42 -0700

    clean up better

--- Convert_to_CSV.m
+++
@@ -1,6 +0,0 @@
-%Requires five_sens_Rig_Display to be run before
-
-%Take data_matrix from five_sens_Rig_Display and converts to CSV format
-cswrite('DataLog_CSV.dat',data_matrix)
-
-%This file can be opened and read in Excel
