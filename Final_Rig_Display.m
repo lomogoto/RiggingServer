d7930c18423a72664ba971b0e301dd9115298aba
Brad Tucker <brad.victor.tucker@gmail.com>
2018-04-21 19:48:39 -0700

    revert axis

--- Final_Rig_Display.m
+++ Final_Rig_Display.m
@@ -4,8 +4,8 @@ clearvars
 
 %initialize data
 xdata = [0, 0, 0, 0, 0, 0];
-ydata = [0, 1, 2, 2, 1, 0];
-zdata = [-.25, -.25, -.25, .25, .25, .25];
+ydata = [-.25, -.25, -.25, .25, .25, .25];
+zdata = [0, 1, 2, 2, 1, 0];
 
 %log data into one matrix
 data_log = [0, 180];
@@ -15,18 +15,18 @@ figure('Position', [100, 100, 700, 700])
 
 %plot data in three dimentions
 subplot(1,1,1)
-fig1 = plot3(zdata, xdata, ydata, 'b-o');
-xlabel('z')
-ylabel('x')
-zlabel('y')
+fig1 = plot3(xdata, ydata, zdata, 'b-o');
+xlabel('x')
+ylabel('y')
+zlabel('z')
 axis([-2, 2, -2, 2, 0, 4])
 grid on
 
 
 %set data sources to update 3D plot
-fig1.XDataSource = 'zdata';
-fig1.YDataSource = 'xdata';
-fig1.ZDataSource = 'ydata';
+fig1.XDataSource = 'xdata';
+fig1.YDataSource = 'ydata';
+fig1.ZDataSource = 'zdata';
 
 %add current path to python to load client
 if count(py.sys.path, pwd) == 0
@@ -48,9 +48,9 @@ while running
     x1 = client.get('rf', 0);
     y1 = client.get('rf', 1);
     z1 = client.get('rf', 2);
-    x2 = client.get('lf', 0);
-    y2 = client.get('lf', 1);
-    z2 = client.get('lf', 2);
+    x2 = client.get('rt', 0);
+    y2 = client.get('rt', 1);
+    z2 = client.get('rt', 2);
  
     %calculate rotation matrix for each data value
     rx1 = [1, 0, 0; 0, cosd(x1), -sind(x1); 0, sind(x1), cosd(x1)];
@@ -63,15 +63,15 @@ while running
     rz2 = [cosd(z2), -sind(z2), 0; sind(z2), cosd(z2), 0; 0, 0, 1];
     
     %calculate vector 1 for left femur
-    lfv = rx1*rz1*[0;1;0];
+    lfv = [1,0,0;0,0,1;0,-1,0]*rx1*ry1*rz1*[0;0;-1];
 
     %calculate vector 2 for right femur
-    rfv = rx1*rz2*[0;1;0];
-        
+    rfv = [0;0;-1];%rx2*ry2*rz2*[0;0;-1];
+    
     %update 3D plot data
     xdata = [0, 0, 0, 0, lfv(1), rfv(1)+lfv(1)];
-    ydata = [0, 1, 2, 2, 2-lfv(2), 2-rfv(2)-lfv(2)];
-    zdata = [.25, .25, .25, -.25, -.25+lfv(3), -.25+rfv(3)+lfv(3)];
+    ydata = [-.25, -.25, -.25, .25, .25+lfv(2), .25+rfv(2)+lfv(2)];
+    zdata = [0, 1, 2, 2, 2+lfv(3), 2+rfv(3)+lfv(3)];
 
     
     %try to update and draw to figure
